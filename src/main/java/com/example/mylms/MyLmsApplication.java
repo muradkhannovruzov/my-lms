95498faa1e69fc13af1f27dcb738d85cae1ef921
muradxan <muradkhannovruzov@gmail.com>
2023-02-27 23:46:53 +0400

    First commit

---
+++ src/main/java/com/example/mylms/MyLmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mylms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mylms/MyLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mylms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
