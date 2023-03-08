cd2bb6525d13b07f05196d77fbe4280971d24f36
muradxan <muradkhannovruzov@gmail.com>
2023-03-08 15:29:31 +0400

    Created jwt

--- src/main/java/com/example/mylms/controller/WelcomeController.java
+++ src/main/java/com/example/mylms/controller/WelcomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/welcome")
+@RequestMapping("/api/v1/welcome")
 public class WelcomeController {
 
     @GetMapping
